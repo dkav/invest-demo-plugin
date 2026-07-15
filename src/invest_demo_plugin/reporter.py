f3f5ac139aa92fc71fb542447f393ee455ef0c52
Emily Soth <esoth@stanford.edu>
2026-07-15 08:54:12 -0700

    update for invest 3.19.0

--- src/invest_demo_plugin/reporter.py
+++ src/invest_demo_plugin/reporter.py
@@ -48,7 +48,7 @@ def report(file_registry: dict, args_dict: dict, model_spec: ModelSpec,
     section_header = f'Multiplication factor {args_dict["factor"]}'
 
     input_raster_stats_table = raster_utils.raster_inputs_summary(
-        args_dict).to_html(na_rep='')
+        args_dict, model_spec).to_html(na_rep='')
 
     output_raster_stats_table = raster_utils.raster_workspace_summary(
         file_registry).to_html(na_rep='')
